3dafb9802b4bd66c1ac19ec9882b2538772dbd71
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2024-02-28 14:24:21 +0100

    fixed issue with JS
    Couldnt expand any questions but the first one, so I made some changes to the JS to make it work.

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const plussIcon = document.querySelector(".question-btn");
-const paragraph = document.querySelector(".paragraph");
+// const plussIcon = document.querySelector(".question-btn");
+// const paragraph = document.querySelector(".paragraph");
 
-function expandQuestion() {
+// function expandQuestion() {
+//   paragraph.classList.toggle("invisible");
+// }
+
+// plussIcon.addEventListener("click", expandQuestion);
+
+// const plussIcons = document.querySelectorAll(".question-img");
+// const paragraphs = document.querySelectorAll(".paragraph");
+
+// function expandQuestion(event) {
+//   const paragraph = event.currentTarget.nextElementSibling;
+//   paragraph.classList.toggle("invisible");
+// }
+
+// plussIcons.forEach((plussIcon, index) => {
+//   plussIcon.addEventListener("click", () => expandQuestion(paragraphs[index]));
+// });
+
+const plussIcons = document.querySelectorAll(".question-img");
+const paragraphs = document.querySelectorAll(".paragraph");
+
+function expandQuestion(event) {
+  const paragraph = event.currentTarget.parentNode.nextElementSibling;
   paragraph.classList.toggle("invisible");
 }
 
-plussIcon.addEventListener("click", expandQuestion);
+plussIcons.forEach((plussIcon, index) => {
+  plussIcon.addEventListener("click", () => expandQuestion(event));
+});
